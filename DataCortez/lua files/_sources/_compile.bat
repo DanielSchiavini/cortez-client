9d50bbbca1ae75665aeb862eaad0fc5c8326500e
danielsc99 <danielsc99@e724f736-04db-794b-b648-ee64ee4c4e6e>
2011-05-15 22:06:33 +0000

    moving lua files sources
    git-svn-id: https://svn.cortezonline.org/svn/Cortez/bRO/trunk@572 e724f736-04db-794b-b648-ee64ee4c4e6e

---
+++ DataCortez/lua files/_sources/_compile.bat
@@ -0,0 +1,4 @@
+@echo off
+..\..\..\..\Outros\Applications\Lua\luac5.0.2.exe -o ..\skillinfo\skilltreeview.lub skillinfo\skilltreeview.lua
+echo skilltreeview.lua compiled to skilltreeview.lub
+pause
